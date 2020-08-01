a8e76bad717db98cf377dc90e4c745c48b00858f
MRManohar <lmmrider99@gmail.com>
2020-08-01 10:37:59 +0530

    FSJ0420A109

--- src/dao/BlogDaoInterface.java
+++ src/dao/BlogDaoInterface.java
@@ -5,12 +5,8 @@ import java.util.List;
 
 import model.Blog;
 
-public interface BlogDaoInterface {
-	
-		void insertBlog(Blog blog) throws SQLException;
-		Blog selectBlog(int blogid);
-	List<Blog> selectAllBlogs();
-		boolean deleteBlog(int id) throws SQLException;
-		boolean updateBlog(Blog blog) throws SQLException, Exception;
-	
+interface BlogDaoInterface
+{
+	void insertBlog(Blog blog) throws SQLException, Exception;
+	List selectAllBlogs() throws SQLException, Exception;
 }
